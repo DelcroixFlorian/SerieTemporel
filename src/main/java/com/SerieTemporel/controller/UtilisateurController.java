77cd42e94154bf6f8ed6e01124cc140782a83e23
Dev#19986458 <florian.delcroix98@gmail.com>
2022-10-20 11:06:17 +0200

    add controllerUser
    essai persistence

---
+++ src/main/java/com/SerieTemporel/controller/UtilisateurController.java
@@ -0,0 +1,27 @@
+
+
+package com.SerieTemporel.controller;
+
+import java.util.ArrayList;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.SerieTemporel.modele.Utilisateur;
+
+
+public class UtilisateurController {
+    
+    
+   /*  @GetMapping("/utilisateurs")
+    public ArrayList<Utilisateur> getUtilisateurs(){
+
+    }
+
+    @getMapping("/{userid}")
+    public Utilisateur getUtilisateur(@PathVariable(value = "userid") long userid){
+
+    }*/
+}
