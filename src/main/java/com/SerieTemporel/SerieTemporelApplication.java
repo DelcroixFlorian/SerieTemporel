9819ec119d7c2073032cc17da1e980fe507593f3
Dev#19986458 <florian.delcroix98@gmail.com>
2022-10-20 09:57:33 +0200

    Initial commit

---
+++ src/main/java/com/SerieTemporel/SerieTemporelApplication.java
@@ -0,0 +1,13 @@
+package com.SerieTemporel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerieTemporelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerieTemporelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SerieTemporel/SerieTemporelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SerieTemporel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerieTemporelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
