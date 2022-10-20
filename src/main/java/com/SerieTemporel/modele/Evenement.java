6d93cde2f420ccd4b3a3571a31737577ec6997c1
Gauvin Alexandre <alexandre.gauvin@iut-rodez.fr>
2022-10-20 11:43:34 +0200

    Compile 11 et annotations event

--- src/main/java/com/SerieTemporel/modele/Evenement.java
+++ src/main/java/com/SerieTemporel/modele/Evenement.java
@@ -1,16 +1,29 @@
 package com.SerieTemporel.modele;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.sql.Date;
 
+@Entity
 public class Evenement {
-    
+
+    @Id
     private long id_event;
+
     private long id_serie;
     private Date date;
     private double valeur;
     private String etiquette;
     private String commentaire;
 
+    /**
+     *
+     * @param id_serie
+     * @param date
+     * @param valeur
+     * @param etiquette
+     * @param commentaire
+     */
     public Evenement(long id_serie,Date date,double valeur,String etiquette,String commentaire){
         this.id_serie = id_serie;
         this.date = date;
@@ -19,5 +32,8 @@ public class Evenement {
         this.commentaire = commentaire;
     }
 
-    
+
+    public Evenement() {
+
+    }
 }
