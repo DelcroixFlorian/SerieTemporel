573a45c966c05bc96eefafba0eb060b9ca5a87cc
Dev#19986458 <florian.delcroix98@gmail.com>
2022-11-30 15:06:31 +0100

    model test repo
    model pour le test des repo

--- src/test/java/com/SerieTemporel/testRepository/testRepoSerie.java
+++ src/test/java/com/SerieTemporel/testRepository/testRepoSerie.java
@@ -1,5 +1,30 @@
 package com.SerieTemporel.testRepository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import com.SerieTemporel.modele.Utilisateur;
+import com.SerieTemporel.repository.UtilisateurRepo;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@DataJpaTest
 public class testRepoSerie {
     
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    UtilisateurRepo userRepo;
+
+    @Test
+    public void saveUser(){
+        Utilisateur user = new Utilisateur();
+        user = entityManager.persistAndFlush(user);
+
+        assertThat(userRepo.findById(user.getId()).get()).isEqualTo(user);
+    }
 }
